1d78b38eab753b6b27b06d172b4e5ba1ff7ddc31
Tiago Teixeira Santos <tiagots86@hotmail.com>
2024-11-28 19:26:33 -0300

    correções da estrutura de arquivos

--- deployment/main.tf
+++ deployment/main.tf
@@ -7,7 +7,7 @@ module "vpc" {
 
   region = var.region
 
-  availabilty_zones = var.availability_zones
-  public_subnets    = var.public_subnets
-  private_subnets   = var.private_subnets
+  availability_zones = var.availability_zones
+  public_subnets     = var.public_subnets
+  private_subnets    = var.private_subnets
 }
